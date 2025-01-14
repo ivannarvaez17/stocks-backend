eff27993bc231795cf5487e70e044a3c5fe3fc1f
Ivan <ivannarvaez17@gmail.com>
2025-01-14 19:17:27 -0400

    Initialized repo with folders and basic files

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+const app = express()
+/*const stocksControlle = require('./serti/stocks')
+
+app.use('/users', usersController)
+*/
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000')
+})
